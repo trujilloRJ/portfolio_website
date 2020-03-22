85d67306eca1faad221a5537c06d48c1a762c1a8
unknown <trujillorodriguezjavier@gmail.com>
2020-03-22 21:53:38 +0100

    redaction

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -27,24 +27,21 @@ export default function Bio() {
         <div className="grid g2">
           <CardText props={cardTextProps}>
             <div className={styles.bio__text}>
-              I like data and extracting meaningful information from it. This
-              pushed me into pursuing a Ph.D. in Radar and Remote Sensing. But
-              this also made that I become interested in
-              <span className="txt-focus"> Data Science</span> and worked as a
-              data scientist for the telecom industry for almost a year. During
-              that time, I've learned that no matter how accurate your machine
-              learning model is or how good are the insights you extract from
-              raw data if you don't show it and explaining it to the world in
-              simple terms. I had learned the power of data visualization and I
-              started to look into web User Interfaces. I have been learning
+              The concept of extracting meaningful information from data have
+              always caught my attention. It pushed me into pursuing a Ph.D. in
+              Radar and Remote Sensing and also took me into
+              <span className="txt-focus"> Data Science</span>. I worked as a
+              data scientist for the telecom industry for almost a year and
+              during that time, I've learned that no matter how accurate your
+              machine learning model is, or how good the insights you extract
+              from data are if you don't show it and explaining it to the world
+              in simple terms. I'd understood the power of data visualization
+              which motivated me to develop web User Interfaces using
               <span className="txt-focus"> HTML, CSS, JavaScript </span>
-              and JS frameworks like <span className="txt-focus">
-                {" "}
-                React{" "}
-              </span>{" "}
-              by developing apps. Therefore, if you have any dataset that needs
-              to be analyzed and/or visualized or you just need a modern,
-              responsive website where you can tell/sell something
+              and JS frameworks like <span className="txt-focus"> React</span>.
+              Therefore, if you have any dataset that needs to be analyzed
+              and/or visualized or you just need a modern, responsive website
+              where you can tell/sell something
               <span className="txt-focus"> I am your guy </span>.
             </div>
           </CardText>
