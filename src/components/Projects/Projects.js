9340e7dcb44516578554f1ba7b5d9c17b8532782
unknown <trujillorodriguezjavier@gmail.com>
2020-04-24 19:56:09 +0200

    new project coronaStats

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,33 +5,43 @@ import CardWork from "../CardWork/CardWork";
 import GatsbyBlocks from "../SVG/GatsbyBlocks";
 import ScubaCorals from "../SVG/ScubaCorals";
 import CocktailWorld from "../SVG/CocktailWorld";
+import CoronaStats from "../SVG/CoronaStats";
 import html from "../../images/html.png";
 import css from "../../images/css.png";
 import js from "../../images/js.png";
 import react from "../../images/react.png";
 import sass from "../../images/sass.png";
 import gatsby from "../../images/gatsby.png";
+import angular from "../../images/angular.png";
+
+const cardWorkProps0 = {
+  text:
+    "A daily-updated Coronavirus dashboard that provides quick access to complete data for all the countries affected by the disease.",
+  linkCode: "https://github.com/trujilloRJ/coronaStats",
+  linkWeb: "https://www.virusdata.dev/home",
+  techList: [angular, js, sass],
+};
 
 const cardWorkProps1 = {
   text:
     "A web application to get cocktail and beverage data from a third-party API. Includes over 200 beverage recipes",
   linkCode: "https://github.com/trujilloRJ/cocktail_world",
   linkWeb: "https://trujillorj.github.io/cocktail_world/",
-  techList: [react, js, sass]
+  techList: [react, js, sass],
 };
 const cardWorkProps2 = {
   text:
     "A static website to promote a real Airbnb diving experience. It's a simple website with a clear call to action and Airbnb integration for payments and schedule",
   linkCode: "https://github.com/trujilloRJ/diving_website",
   linkWeb: "https://www.scubacorals.com/",
-  techList: [html, css, js]
+  techList: [html, css, js],
 };
 const cardWorkProps3 = {
   text:
     "A set of react components to integrate with the static site generator Gatsby. It includes stateless components for improved productivity",
   linkCode: "https://github.com/trujilloRJ/gatsby_blocks_v1",
   linkWeb: "https://competent-bassi-22acff.netlify.com/",
-  techList: [gatsby, sass, js, react]
+  techList: [gatsby, sass, js, react],
 };
 
 export default function Projects() {
@@ -39,7 +49,10 @@ export default function Projects() {
     <div id="projects" className={styles.projects}>
       <div className="container">
         <SectionHeading text="Projects" />
-        <div className="grid g3">
+        <div className="grid g2">
+          <CardWork props={cardWorkProps0}>
+            <CoronaStats width={250} height={68} />
+          </CardWork>
           <CardWork props={cardWorkProps1}>
             <CocktailWorld width={250} height={68} />
           </CardWork>
